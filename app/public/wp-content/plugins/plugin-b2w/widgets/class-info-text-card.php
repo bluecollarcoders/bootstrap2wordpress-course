8e79d826ca3607f4c07bc0be99d8fec0dd0bf2ef
Caleb Matteis <matteismusic@gmail.com>
2022-09-08 10:51:15 -0400

    add info-text card

--- app/public/wp-content/plugins/plugin-b2w/widgets/class-info-text-card.php
+++ app/public/wp-content/plugins/plugin-b2w/widgets/class-info-text-card.php
@@ -8,7 +8,7 @@ class B2W_Info_Text_Card_Widget extends \Elementor\Widget_Base {
 
     public function get_name()
     {
-        return 'b2w_info_text_card';
+        return 'b2w_text_card';
     }
 
     public function get_title()
@@ -18,7 +18,7 @@ class B2W_Info_Text_Card_Widget extends \Elementor\Widget_Base {
 
     public function get_icon()
     {
-        return 'eicon-editor-card';
+        return 'eicon-info-box';
     }
 
     public function get_keywords()
@@ -33,16 +33,72 @@ class B2W_Info_Text_Card_Widget extends \Elementor\Widget_Base {
     protected function register_controls()
     {
         $this->start_controls_section(
-            'b2w_card_text_info',
+            'b2w_text_info',
             [
-                'label' => __( 'Link', 'plugin-b2w'),
+                'label' => __( 'Info Card', 'plugin-b2w'),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'info_title_text',
+            [
+                'label' => __( 'Title', 'plugin-b2w'),
+                'label_block' => true,
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'placeholder' => __('Title', 'plugin-b2w'),
+                'default' => __('Handcrafted Code', 'plugin-b2w'),
+            ]
+            );
+
+        $this->add_control(
+            'info_desc',
+            [
+              'label' => __('Description', 'plugin-b2w'),
+              'label_block' => true,
+              'type' => \Elementor\Controls_Manager::TEXTAREA,
+              'placeholder' => __('Description', 'plugin-b2w'),
+              'default' => __('Yes, I know "handcrafted" is a word', 'plugin-b2w')
+            ]
+            );
+
+        $this->add_control(
+            'card_image',
+            [
+                'label' => __('Choose Image', 'plugin-b2w'),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [
+                    'url' => \Elementor\Utils::get_placeholder_image_src()
+
+                ],
+            ]
+        );
+
+        $this->add_group_control(
+            \Elementor\Group_Control_Background::get_type(),
+            [
+                'name' => 'background',
+                'label' => __( 'Background', 'plugin-b2w'),
+                'types' => [ 'classic', 'gradient'],
+                'selector' => '{{WRAPPER}} .overlay',
             ]
         );
 
 
         $this->end_controls_section();
     }
+
+        protected function render()
+        {
+            $settings = $this->get_settings_for_display();
+
+        echo '<div class="text-card style-1"> ';
+        echo '<div class="overlay"></div>';
+        echo '<h4>'.$settings['info_title_text'] .'</h4>';
+        echo '<p>'.$settings['info_desc'].'</p>';
+        echo '<div class="overlay-image">img src="'.esc_url($settings['card_image']['url']).'"</div>';
+        echo '</div>';
+        }
 }
 
 // Register widget
